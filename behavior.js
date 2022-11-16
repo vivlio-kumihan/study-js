eb999673239b7322f7b8cb0179d627b7a076daa0
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-11-17 06:00:31 +0900

    first commit

---
+++ behavior.js
@@ -0,0 +1,18 @@
+///////////////////
+// JSの使い方　その1
+// htmlの"body終了タグ直前"に『指定するJSファイルの在処』を記述する。
+// そして、担保として、
+// JS側でも、HTMLドキュメントを読み込んでから『以下の命令』をする一文を作成しておく。
+
+// document.addEventListener("DOMContentLoaded", function(){
+//   document.querySelectior("ul l");
+// });
+
+
+///////////////////
+// JSの使い方　その2
+// htmlのheadの中に記述する。
+// 『defer』属性をつけることでhtmlを読み込んでからJSを効かせる。
+// JSの記述しシンプルになる。
+
+document.querySelectior("ul l");
