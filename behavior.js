ea96786897ab7b01ee3458a9ac163ee56848da6f
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-11-17 06:04:19 +0900

    JSをロードする　その1

--- behavior.js
+++ behavior.js
@@ -4,15 +4,6 @@
 // そして、担保として、
 // JS側でも、HTMLドキュメントを読み込んでから『以下の命令』をする一文を作成しておく。
 
-// document.addEventListener("DOMContentLoaded", function(){
-//   document.querySelectior("ul l");
-// });
-
-
-///////////////////
-// JSの使い方　その2
-// htmlのheadの中に記述する。
-// 『defer』属性をつけることでhtmlを読み込んでからJSを効かせる。
-// JSの記述しシンプルになる。
-
-document.querySelectior("ul l");
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectior("ul l");
+});
