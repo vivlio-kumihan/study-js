0cc23cb766c92eab253bdf067535ada1dbf79084
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-11-26 11:53:06 +0900

    時間　現在の日付を取得する。それをリストで表示する。 => createElement(li)  appendChild(li)

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -778,18 +778,86 @@
 // console.table(item)
 
 
-///////////////////
-//　型
+// ///////////////////
+// //　型
 
-// 型を明示する。　typeof
-let input = document.getElementById("input").textContent
-console.log(typeof input) 
-// => string
-// 文字列の結合になる。
-document.getElementById("output").textContent = input + 1000
+// // 型を明示する。　typeof
+// let input = document.getElementById("input").textContent
+// console.log(typeof input) 
+// // => string
+// // 文字列の結合になる。
+// document.getElementById("output").textContent = input + 1000
 
-// 型を変換する。　Number(ins), String(ins)
-console.log(Number(input))
-input = Number(input)
-// 数ととして計算する。
-document.getElementById("output").textContent = input + 1000
+// // 型を変換する。　Number(ins), String(ins)
+// console.log(Number(input))
+// input = Number(input)
+// // 数ととして計算する。
+// document.getElementById("output").textContent = input + 1000
+
+
+// ///////////////////
+// //　時間
+
+// 現在の日付　インスタンスを生成させる。
+const thisTime = new Date(), 
+      week = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
+
+// 現在時間を表示させる。
+document.querySelector(".today").textContent = thisTime
+
+// 年を抜き出す
+let year = thisTime.getFullYear() + "年"
+// 月を抜き出す。
+let month = thisTime.getMonth() + 1 + "月"
+// 日を抜き出す。
+let day = thisTime.getDate() + "日"
+// 日曜日を0として、各曜日のインデックスを返す。
+let nthDay = week[thisTime.getDay()]
+// 時間（時）を抜き出す。
+let hours = thisTime.getHours() + "時"
+// 時間（分）を抜き出す。
+let minutes = thisTime.getMinutes() + "分"
+// 時間（秒）を抜き出す。
+let seconds = thisTime.getSeconds() + "秒"
+
+// 確認する。
+console.log(thisTime)
+console.log(month)
+console.log(day)
+console.log(nthDay)
+console.log(hours)
+console.log(minutes)
+console.log(seconds)
+
+// 現在の日付を取得する。それをリストで表示する。 => createElement("li") appendChild(li)
+let myDate = [year, month, day, nthDay, hours, minutes, seconds]
+myDate.forEach(elem => {
+  let li = document.createElement("li")
+  li.textContent = elem
+  document.querySelector("ul.list-view").appendChild(li)
+});
+
+// const thisTime = new Date()
+
+// function outPutDate(ins) {
+//   const week = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
+//   let year = ins.getFullYear() + "年"
+//   let month = ins.getMonth() + 1 + "月"
+//   let day = ins.getDate() + "日"
+//   let nthDay = week[ins.getDay()]
+//   let hours = ins.getHours() + "時"
+//   let minutes = ins.getMinutes() + "分"
+//   let seconds = ins.getSeconds() + "秒"
+//   let elem = year + month + day + nthDay + hours + minutes + seconds
+//   return elem
+// }
+
+
+// console.log(outPutDate(thisTime))
+// document.querySelector(".today").textContent = outPutDate(thisTime)
+
+// thisTime.setDate(thisTime.getDate() + 1) 
+// document.querySelector(".tomorrow").textContent = outPutDate(thisTime)
+
+// dayTime = dayTime / 1000 / 60 / 60 / 24
+// console.log(dayTime)
