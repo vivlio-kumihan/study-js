f45d5ab077d24038d1205c946f8e285caf4e2807
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-12-15 08:36:08 +0900

    配列の中身をforEachを使いHTMLのリストで出力する。=> forEach

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,55 +1,19 @@
 //////////////////////////////////////////////////
 // 配列
+// 配列の中身をforEachを使いHTMLのリストで出力する。
 
-// 配列の生成
-// new Array()
-const numArr = new Array(1,2,3)
-const fruit = new Array("apple", "banana", "orange")
-const member = new Array(
-  {
-    name: ["高広", "信之"],
-    age: 57,
-    gender: "male"
-  },
-  {
-    name: ["木田", "紀子"],
-    age: 58,
-    gender: "female"
-  },
-  {
-    name: ["竹中", "丘像"],
-    age: 60,
-    gender: "male"
-  }
-)
-console.log(numArr)
-console.log(fruit)
-console.log(`${member[1].name[0]}${member[1].name[1]}さんは、${member[1].age }歳です。`)
+// 引数は、Element, Index, Arrayを持てる。()で括らないとエラーになる。
+const fruit = ["apple", "banana", "orange", "grape", "melon"];
+fruit.forEach((name, idx, array) => {
+  let li = document.createElement("li");
+  li.textContent = fruit[idx];
+  document.getElementById("fruits").appendChild(li);
+})
 
-// 配列リテラル
-// const fruit = ["apple", "banana", "orange"]
-// console.log(fruit)
-
-// 配列へ要素を追加
-/////  push    array <= (item)
-/////  pop     arra |-> y
-/////  umshift (item) => array
-/////  shift   a <-| rray
-// 配列の最後へ
-fruit.push("grape");
-fruit.push("melon");
-console.log(fruit);
-
-// 配列の最初へ
-fruit.unshift("grape");
-fruit.unshift("melon");
-console.log(fruit);
-
-// 配列の要素を削除
-// 配列の最後
-fruit.pop();
-console.log(fruit);
-
-// 配列の最初
-fruit.shift();
-console.log(fruit);
+// 普通は、Elementだけでだろう。
+const member = ["木田","竹中","高広","小林"]
+member.forEach(element => {
+  let li =document.createElement("li")
+  li.textContent = element
+  document.getElementById("member").appendChild(li)
+})
