5e5dd0c5d36b60c5617ab1ce08f014c1523fc924
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-12-15 16:33:03 +0900

    親要素の操作。=> nextElementSibling

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,11 +1,7 @@
 //////////////////////////////////////////////////
 // 配列
-// 兄弟要素の操作。=> nextElementSibling, previousElementSibling
+// 親要素の操作。=> nextElementSibling
 
-// 次にある、兄弟要素を取得する。　nextElementSibling
-const list1 = document.querySelector(".list li");
-console.log(list1.nextElementSibling);
-
-// 前にある、兄弟要素を取得する。　previousElementSibling
-const list2 = document.querySelector(".list");
-console.log(list2.lastElementChild.previousElementSibling);
+// 親要素を取得する。　nextElementSibling
+const list = document.querySelector(".list li")
+console.log(list.parentNode)
