61de3e9967495f74e4e433c8b88f37e6f6a553c0
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-12-16 10:31:16 +0900

    examples ハンバーガーメニューの展開メニューリスト。フワッと出現 => js:toggle css:opacity, visibility

--- examples/ハンバーガーメニューとドロワーメニュー/assets/js/behavior.js
+++ examples/ハンバーガーメニューとドロワーメニュー/assets/js/behavior.js
@@ -1,6 +1,8 @@
 // for ハンバーガーボタン
 // toggleで効果のかかりをスイッチング
-const menuButton = document.getElementById("menuButton")
+const menuButton = document.getElementById("menuButton"),
+      menu = document.getElementById("menu")
 menuButton.addEventListener("click", () => {
   menuButton.classList.toggle("active")
+  menu.classList.toggle("active")
 })
