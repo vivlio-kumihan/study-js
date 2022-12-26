4dbd14f5f2eaed59b889810b5e82457bcc9c53ff
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-12-27 08:46:31 +0900

    Swiper　CSSからイメージを読み込ませる。

--- examples/スライド/assets/js/behavior.js
+++ examples/スライド/assets/js/behavior.js
@@ -4,18 +4,21 @@ const swiper = new Swiper('.swiper', {
 
   speed: 1000,
   
+  // 自動で再生
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
 
+  // ページネーションをクリックして該当写真まで移動
   pagination: {
     el: '.swiper-pagination',
     clickable: true
   },
 
+  // ナビゲーションのタブを表示
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   }
-});
+})
