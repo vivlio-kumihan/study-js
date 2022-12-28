121351714280e0204d84231891558e8d82ea0f55
Nobuyuki Takahiro <studio.quad9@gmail.com>
2022-12-28 15:46:43 +0900

    1つのHTMLに複数のスライダー実装

--- examples/スライド/assets/js/behavior.js
+++ examples/スライド/assets/js/behavior.js
@@ -1,24 +1,6 @@
-
-
-const swiper = new Swiper('.swiper', {
+const firstSwiper = new Swiper('.swiper.first', {
   // Optional parameters
   loop: true,
-  slidesPerView: 1,
-  // 単位はピクセル。単位は書かない。
-  // spaceBetween: 15,
-  // 一番最初のスライドをセンターに置く。
-  centeredSlides: true,
-  // レスポンシブル対応 breakpoints
-  breakpoints: {
-    769: {
-      slidesPerView: 3,
-      spaceBetween: 15,
-    },
-    1025: {
-      slidesPerView: 5,
-      spaceBetween: 0,
-    }
-  },
   // 1秒かけて移遷する。
   speed: 1000,
   // 自動で再生
@@ -32,13 +14,41 @@ const swiper = new Swiper('.swiper', {
 
   // ページネーションをクリックして該当写真まで移動
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination.first',
     clickable: true
   },
 
   // ナビゲーションのタブを表示
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next first',
+    prevEl: '.swiper-button-prev first',
+  }
+})
+
+const secondSwiper = new Swiper('.swiper.second', {
+  // Optional parameters
+  loop: true,
+  effect: 'fade',
+  // 1秒かけて移遷する。
+  speed: 1000,
+  // 自動で再生
+  autoplay: {
+    // 2.5秒で自動送りをする。
+    delay: 2500,
+    // autoplay中にページネーションを触ったりすると自動再生は中止される。
+    // 回避するオプションが、disableOnInteraction。
+    disableOnInteraction: false,
+  },
+
+  // ページネーションをクリックして該当写真まで移動
+  pagination: {
+    el: '.swiper-pagination.second',
+    clickable: true
+  },
+
+  // ナビゲーションのタブを表示
+  navigation: {
+    nextEl: '.swiper-button-next second',
+    prevEl: '.swiper-button-prev second',
   }
 })
