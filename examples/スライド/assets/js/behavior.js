d5ecee46302b16ef8324f2e1fa251f93408b9df0
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-06 17:02:07 +0900

    Swiper スライド上に文字をレイアウトする。
    スライド上で位置が変わらない文字をレイアウトする場合、
    スライダーをラップしてposition: relative <=> absoluteで指示する。

--- examples/スライド/assets/js/behavior.js
+++ examples/スライド/assets/js/behavior.js
@@ -3,16 +3,9 @@ const swiper = new Swiper('.swiper', {
   // 自動でループ挿せる際に、最後のスライドから最初へ遷移でバグがある。
   // それの回避方法が『loop: true』
   loop: true,
-  effect: 'fade',
-  // どれくらいの時間をかけて切り替わるかを調整する。
-  speed: 3000,
-  // 自動で再生
+  speed: 1000,
   autoplay: {
     delay: 1000,
-    // ユーザが操作しても自動再生を継続させることができる。
-    // デフォルトでは、『true』。
-    // ユーザのアクションがあったときに自動再生を
-    // disable（＝無効化）するように設定されている。
     disableOnInteraction: false,
   },
 
