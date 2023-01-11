ec9eb1f1539ef7e47917a574f1fffffca057c9db
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-11 12:52:28 +0900

    Swiper スライドが立方体のオブジェクト
    ・<div class="swiper-wrapper"></div>必須。HTMLの構造を間違えると写真を縦に並べただけになる。注意が必要。
    ・background-size: cover;とobject-fit: cover;　ケースによってトリミングの効果が変わる？　なぜ？
    ・Swiperにコンテナは必要ない。

--- examples/スライド/assets/js/behavior.js
+++ examples/スライド/assets/js/behavior.js
@@ -1,38 +1,26 @@
-const swiper = new Swiper('.swiper', {
-  effect: 'coverflow',
-  // 一度に見えるスライドの数を設定する。
-  // 『CSSのpadding』でスライドが見切れてしまうのを回避することを忘れずに。
-  slidesPerView: 3,
-  // 先頭のスライドをセンター位置にする。
-  centeredSlides: true,
-  coverflowEffect: {
-    // 両サイドのスライドがトップスライドと重なる箇所に影を入れる。デフォルトは『true』
-    slideShadows: true,
-    // 両サイドのスライドの角度　デフォルトは『50』
-    rotate: 20,
-    // スライドの間隔を狭める　デフォルトは『0』
-    stretch: 100,
-    // スライドの列の奥行き
-    depth: 50,
-    // スライドを回転させる
-    modifier: 8
+const swiper = new Swiper(".swiper", {
+  effect: 'cube',
+  cubeEffect: {
+    shadow: true,
+    slideShadow: true,
+    shadowOffset: 100,
+    shadowScale:0.9
   },
-  loop: true,
+  // 『効果』のスピードを変えるオプション
   speed: 1000,
+  loop: true,
   autoplay: {
-    delay: 1000,
-    disableOnInteraction: false,
+    delay: 2500,
+    disableOnInteraction: false
   },
 
-  // ページネーションをクリックして該当写真まで移動
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+    el: ".swiper-pagination",
+    clickable: true
   },
 
-  // ナビゲーションのタブを表示
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   }
 })
